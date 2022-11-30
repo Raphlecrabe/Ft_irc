ae7394fb97a778e2a853a546eddda2bcc993b45a
Azathothep <felixbelthoise@yahoo.fr>
2022-11-30 15:41:52 +0100

    RPLWELCOME

---
+++ srcs/AReply/RPL_Welcome.cpp
@@ -0,0 +1,25 @@
+#include "../../incs/AReply/RPL_Welcome.hpp"
+#include "../../incs/AReply/AReply.hpp"
+
+RPL_Welcome::RPL_Welcome() : AReply("RPL_WELCOME") {
+
+}
+
+RPL_Welcome::~RPL_Welcome() {
+	
+}
+
+Message RPL_Welcome::getmsg(Hub &hub, Message &message) {
+	std::string client = message.getSender()->getNickname();
+	std::string nick = message.getSender()->getNickname();
+
+	std::string params = client + " :Welcome to the " + hub.getNetworkName() + " Network, " + nick;
+
+	Message	newmessage(hub.getServerName(), "001", params);
+	(void) message;
+
+	newmessage.addDestinator(message.getSender());
+
+	//lol
+	return (newmessage);
+}
