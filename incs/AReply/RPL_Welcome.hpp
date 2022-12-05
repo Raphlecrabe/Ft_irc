aed4b53d2d464ac4721986281b05f74852925059
Raphael Monachon <rmonacho@z4r2p1.42lyon.fr>
2022-12-05 11:06:01 +0100

    merge

---
+++ incs/AReply/RPL_Welcome.hpp
@@ -0,0 +1,18 @@
+#ifndef RPL_WELCOME_HPP
+# define RPL_WELCOME_HPP
+
+# include "AReply.hpp"
+
+# include <iostream>
+
+class RPL_Welcome : public AReply {
+	private:
+
+	public:
+		RPL_Welcome();
+		~RPL_Welcome();
+
+		Message getmsg(Hub &hub, Message &messsage, std::string param);
+};
+
+#endif
