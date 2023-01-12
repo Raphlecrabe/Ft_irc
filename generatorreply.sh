96a43976b73ab02255f1d7cd2108cec486d02c6e
Azathothep <felixbelthoise@yahoo.fr>
2023-01-12 15:47:50 +0100

    Revert "removed generators"
    This reverts commit 5c0b6257d2ca6b3d39ad4c3953cc2feea4b19803.

---
+++ generatorreply.sh
@@ -0,0 +1,22 @@
+Class=$1
+CLASS=$(echo $Class | tr 'a-z' 'A-Z')
+folder=AReply/
+HPP=incs/$folder/$Class.hpp
+CPP=srcs/$folder/$Class.cpp
+cp incs/$folder/SampleHeaderreply.hpp $HPP
+sed -i '' "s/NAME/$CLASS/g" $HPP # $CPP
+sed -i '' "s/Name/$Class/g" $HPP # $CPP
+
+sed -i '' "s/#NEW_REPLY_HERE/$Class.hpp \\\#NR/g" Makefile
+gsed -i "s/#NR/\n\t\t\t\t#NEW_REPLY_HERE/g" Makefile
+
+class=$(echo $Class | tr 'A-Z' 'a-z')
+
+sed -i '' "s=//insert_here=insert(\&$class);//NR=g" srcs/ReplyCreator.cpp
+gsed -i "s=//NR=\n\t//insert_here=g" srcs/ReplyCreator.cpp
+
+sed -i '' "s=//reply_here=$Class $class;//NR=g" incs/ReplyCreator.hpp
+gsed -i "s=//NR=\n\t//reply_here=g" incs/ReplyCreator.hpp
+
+sed -i '' "s=//include_here=# include \"$Class.hpp\"//NR=g" incs/AReply/Includereply.hpp
+gsed -i "s=//NR=\n//include_here=g" incs/AReply/Includereply.hpp
